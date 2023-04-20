7327a51a153ef5114000a1db43d722023808090c
Nathan Gordon <gordonnl@gmail.com>
2023-04-20 14:57:30 +0200

    turtle move

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,5 +4,15 @@ function scrollToID(id) {
 }
 
 // Turtle
-{
-}
+window.addEventListener('load', () => {
+    const turtle = document.querySelector('.tortue-little');
+    console.log(turtle);
+    requestAnimationFrame(animate);
+    let x = 0;
+    function animate(t) {
+        x--;
+        if (x < -1000) x = 0;
+        turtle.style.transform = `translateX(${x}px)`;
+        requestAnimationFrame(animate);
+    }
+});
